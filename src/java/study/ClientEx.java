c851257a3784f748e4c63bfacfd213df7c4d3c03
chriskim <dongil.kim84@gmail.com>
2023-06-14 22:30:22 +0900

    Update ClientEx.java

--- src/java/study/ClientEx.java
+++ src/java/study/ClientEx.java
@@ -11,5 +11,13 @@ public class ClientEx {
     
     System.out.println(contentResponse.getContentAsString());
     
+    ContentResponse res = client.newRequest("http://127.0.0.1:8080/mypath")
+                                .method(HttpMethod.POST)
+                                .header(HttpHeader.CONTENT_TYPE, "application/json")
+                                .content(new StringContentProvider("{\"username\":\"jliu\",\"password\":\"123456\"}", "utf-8"))
+                                .send();
+		
+		System.out.println(res.getContentAsString());
+    
   }
 }
