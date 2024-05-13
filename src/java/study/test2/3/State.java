3d342a1bae2a7901983753fcce5c8f7f162ccbcb
chriskim <dongil.kim84@gmail.com>
2024-05-13 22:53:11 +0900

    Create State.java

---
+++ src/java/study/test2/3/State.java
@@ -0,0 +1,16 @@
+public abstract class State {
+
+	private String name;
+
+	public State(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public abstract void run() throws Exception;
+
+}
